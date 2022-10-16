f1fcc3be25b3dc6590c9bf2e6c05874f290ae45e
Chetan R. Patil <chetanrp@BLR.ALLEGISINDIA.COM>
2022-10-16 19:48:58 +0530

    Single User

--- src/pages/single/Single.js
+++ src/pages/single/Single.js
@@ -1,8 +1,51 @@
 import React from "react";
+import Navbar from "../../components/navbar/Navbar";
+import Sidebar from "../../components/sidebar/Sidebar";
 import "./single.scss";
 
 const Single = () => {
-  return <div>Single</div>;
+  return (
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
+            <div className="editBtn">Edit</div>
+            <h2 className="title">Information</h2>
+            <div className="item">
+              <img
+                className="image"
+                src="https://th.bing.com/th/id/OIP.FlCvDiM99v7ghRF05mpjaAHaEK?pid=ImgDet&rs=1"
+                alt="image"
+              />
+              <div className="details">
+                <h1 className="itemTitle">Professor</h1>
+                <div className="detailItem">
+                  <span className="itemKey">Email ID:</span>
+                  <span className="itemValue">professor@gmail.com</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Phone Number:</span>
+                  <span className="itemValue">+1 999 888 766</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Address:</span>
+                  <span className="itemValue">NY - Street 223</span>
+                </div>
+                <div className="detailItem">
+                  <span className="itemKey">Country:</span>
+                  <span className="itemValue">US</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="right"></div>
+        </div>
+        <div className="bottom"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Single;
